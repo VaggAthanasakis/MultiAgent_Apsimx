57eb3db862392221c29078c2cf4b664fc24416d3
VaggAthanasakis <athanasakis.evangelos@outlook.com>
2025-03-12 20:47:26 +0200

    Weather Tool Updated for Retrieving Forcast Data

--- weather_data_retriever.py
+++ weather_data_retriever.py
@@ -5,6 +5,7 @@ import requests_cache
 import pandas as pd
 from retry_requests import retry
 import re
+from datetime import datetime
 
 class OpenMeteoWeatherDownloader:
     def __init__(self, location, latitude, longitude,
@@ -42,8 +43,11 @@ class OpenMeteoWeatherDownloader:
         export a CSV file, and create an INI file with the location information.
         """
         # Define API URL and parameters
-        url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
-        params = {
+        
+        historical_url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+        forcast_url =  "https://api.open-meteo.com/v1/forecast"
+
+        historical_params = {
             "latitude": self.latitude,
             "longitude": self.longitude,
             "start_date": self.start_date,
@@ -55,99 +59,175 @@ class OpenMeteoWeatherDownloader:
             "daily": ["temperature_2m_max", "temperature_2m_min", "rain_sum", "shortwave_radiation_sum"]
         }
 
-        # Request weather data (assuming the API returns a list of responses)
-        responses = self.openmeteo.weather_api(url, params=params)
-        response = responses[0]  # Process first location. Use a loop if needed.
+        forecast_params = {
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            #"forecast_days": difference,
+            "hourly": [
+                "wind_speed_10m", "soil_temperature_0cm",
+                "relative_humidity_2m", "vapour_pressure_deficit"
+            ],
+            "daily": ["temperature_2m_max", "temperature_2m_min", "rain_sum", "shortwave_radiation_sum"]
+        }
 
-        # --------------------
-        # Process Hourly Data
-        # --------------------
-        hourly = response.Hourly()
-        # The order of variables should match the order requested above.
-        hourly_wind_speed_10m = hourly.Variables(0).ValuesAsNumpy()
-        hourly_soil_temperature_0cm = hourly.Variables(1).ValuesAsNumpy()
-        #hourly_direct_radiation = hourly.Variables(2).ValuesAsNumpy()
-        hourly_relative_humidity_2m = hourly.Variables(2).ValuesAsNumpy()
-        hourly_vapour_pressure_deficit = hourly.Variables(3).ValuesAsNumpy()
+        # Check if the end date is a fuure date -> Then have to call the forecast api
+        end_date = self.end_date
+        start_date = self.start_date
+        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+
+        # Get the current date
+        current_date = datetime.today().date()
+
+        # Calculate the difference in days
+        urls = []
+        params = []
         
+        # only historical
+        if end_date <= current_date:
+            print("Only Historical Data")
+            urls = [historical_url]
+            params = [historical_params]
+            historical_df = pd.DataFrame()
+            dataframes_array = [historical_df]
+        # only forecast
+        elif start_date >= current_date:
+            print("Only Forecast")
+            difference = (end_date - current_date).days
+            if difference > 14:
+                difference = 14
+            forecast_params["forecast_days"] = difference
+            urls = [forcast_url]
+            params = [forecast_params]
+            forcast_df = pd.DataFrame()
+            dataframes_array = [forcast_df]
+        # historical + forecast
+        else:
+            difference = (end_date - current_date).days
+            if difference > 14:
+                difference = 14
+            forecast_params["forecast_days"] = difference
 
-        num_values = len(hourly_relative_humidity_2m)
-        hourly_dates = pd.date_range(
-            start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-            periods=num_values,  # Use the number of data points
-            freq=pd.Timedelta(seconds=hourly.Interval())
-        )
+            self.end_date = datetime.now()
+            self.end_date = self.end_date.strftime(("%Y-%m-%d"))
+            print(self.end_date)
+            historical_params["end_date"] = self.end_date
 
-        hourly_data = {
-            "date": list(hourly_dates),
-            "rhmean": hourly_relative_humidity_2m,
-            "windspeed": hourly_wind_speed_10m,
-            "soilt": hourly_soil_temperature_0cm,
-            "vp_deficit": hourly_vapour_pressure_deficit
-        }
-        hourly_dataframe = pd.DataFrame(data=hourly_data)
+            print("HISTORICAL + FORECAST\n")
+            print(f"Days difference: {difference}")
+            urls = [historical_url, forcast_url]
+            params = [historical_params, forecast_params]
 
-        # Resample Hourly Data to Daily Data
-        hourly_dataframe['date'] = pd.to_datetime(hourly_dataframe['date'])
-        hourly_dataframe.set_index('date', inplace=True)
+            historical_df = pd.DataFrame()
+            forcast_df = pd.DataFrame()
+            dataframes_array = [historical_df, forcast_df]
 
-        daily_hourly_dataframe = hourly_dataframe.resample('D').agg({
-            'rhmean': 'mean',
-            'windspeed': 'mean',
-            'soilt': 'mean',
-            'vp_deficit': 'mean'
-        }).reset_index()
 
-        # --------------------
-        # Process Daily Data
-        # --------------------
-        daily = response.Daily()
-        daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
-        daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
-        daily_rain_sum = daily.Variables(2).ValuesAsNumpy()
-        daily_mean_temperature = (daily_temperature_2m_max + daily_temperature_2m_min) / 2
-        daily_mean_radiation = daily.Variables(3).ValuesAsNumpy() #* 0.0036
+        for i in range(len(urls)):
+            #print(i)
+            # Request weather data (assuming the API returns a list of responses)
+            responses = self.openmeteo.weather_api(urls[i], params=params[i])
+            response = responses[0]  # Process first location. Use a loop if needed.
 
-        daily_data = {
-            "date": pd.date_range(
-                start=pd.to_datetime(daily.Time(), unit="s", utc=True),
-                end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
-                freq=pd.Timedelta(seconds=daily.Interval()),
-                inclusive="left"
-            ),
-            "maxt": daily_temperature_2m_max,
-            "mint": daily_temperature_2m_min,
-            "rain": daily_rain_sum,
-            "meant": daily_mean_temperature,
-            "radn":daily_mean_radiation
-        }
-        daily_dataframe = pd.DataFrame(data=daily_data)
+            # --------------------
+            # Process Hourly Data
+            # --------------------
+            hourly = response.Hourly()
+            # The order of variables should match the order requested above.
+            hourly_wind_speed_10m = hourly.Variables(0).ValuesAsNumpy()
+            hourly_soil_temperature_0cm = hourly.Variables(1).ValuesAsNumpy()
+            #hourly_direct_radiation = hourly.Variables(2).ValuesAsNumpy()
+            hourly_relative_humidity_2m = hourly.Variables(2).ValuesAsNumpy()
+            hourly_vapour_pressure_deficit = hourly.Variables(3).ValuesAsNumpy()
+            
 
-        # Merge daily data from hourly aggregation with the daily data
-        merged_df = pd.merge(daily_dataframe, daily_hourly_dataframe, on="date", how="outer")
-        merged_df['date'] = pd.to_datetime(merged_df['date'])
+            num_values = len(hourly_relative_humidity_2m)
+            hourly_dates = pd.date_range(
+                start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+                periods=num_values,  # Use the number of data points
+                freq=pd.Timedelta(seconds=hourly.Interval())
+            )
 
-        # Convert the date into year and cumulative day-of-year
-        merged_df['year'] = merged_df['date'].dt.year
-        merged_df['day'] = merged_df['date'].dt.dayofyear
-        merged_df.drop(columns=['date'], inplace=True)
+            hourly_data = {
+                "date": list(hourly_dates),
+                "rhmean": hourly_relative_humidity_2m,
+                "windspeed": hourly_wind_speed_10m,
+                "soilt": hourly_soil_temperature_0cm,
+                "vp_deficit": hourly_vapour_pressure_deficit
+            }
+            hourly_dataframe = pd.DataFrame(data=hourly_data)
 
-        # Calculate saturation vapor pressure (vp_sat) in kPa
-        merged_df['vp_sat_kPa'] = 0.61078 * np.exp((17.27 * merged_df['meant']) / (merged_df['meant'] + 237.3))
-        # Convert vp_sat to hPa
-        merged_df['vp_sat_hPa'] = merged_df['vp_sat_kPa'] * 10
-        # Convert vp_deficit from kPa to hPa (if your vp_deficit is in kPa)
-        merged_df['vp_deficit_hPa'] = merged_df['vp_deficit'] * 10
-        # Calculate actual vapor pressure (vp) in hPa
-        merged_df['vp'] = merged_df['vp_sat_hPa'] - merged_df['vp_deficit_hPa']
+            # Resample Hourly Data to Daily Data
+            hourly_dataframe['date'] = pd.to_datetime(hourly_dataframe['date'])
+            hourly_dataframe.set_index('date', inplace=True)
 
-        merged_df['vpd'] = merged_df["vp_deficit"]
-        # Drop intermediate columns (optional)
-        merged_df.drop(columns=['vp_sat_kPa', 'vp_sat_hPa', 'vp_deficit_hPa'], inplace=True)
+            daily_hourly_dataframe = hourly_dataframe.resample('D').agg({
+                'rhmean': 'mean',
+                'windspeed': 'mean',
+                'soilt': 'mean',
+                'vp_deficit': 'mean'
+            }).reset_index()
 
-        # Reorder columns as desired
-        new_order = ['year', 'day', 'rain', 'maxt', 'mint', 'meant', 'soilt', 'rhmean', 'vp', 'vpd','windspeed', 'radn']
-        final_df = merged_df[new_order]
+            # --------------------
+            # Process Daily Data
+            # --------------------
+            daily = response.Daily()
+            daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+            daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+            daily_rain_sum = daily.Variables(2).ValuesAsNumpy()
+            daily_mean_temperature = (daily_temperature_2m_max + daily_temperature_2m_min) / 2
+            daily_mean_radiation = daily.Variables(3).ValuesAsNumpy() #* 0.0036
+
+            daily_data = {
+                "date": pd.date_range(
+                    start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+                    end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
+                    freq=pd.Timedelta(seconds=daily.Interval()),
+                    inclusive="left"
+                ),
+                "maxt": daily_temperature_2m_max,
+                "mint": daily_temperature_2m_min,
+                "rain": daily_rain_sum,
+                "meant": daily_mean_temperature,
+                "radn":daily_mean_radiation
+            }
+            daily_dataframe = pd.DataFrame(data=daily_data)
+
+            # Merge daily data from hourly aggregation with the daily data
+            merged_df = pd.merge(daily_dataframe, daily_hourly_dataframe, on="date", how="outer")
+            merged_df['date'] = pd.to_datetime(merged_df['date'])
+
+            # Convert the date into year and cumulative day-of-year
+            merged_df['year'] = merged_df['date'].dt.year
+            merged_df['day'] = merged_df['date'].dt.dayofyear
+            merged_df.drop(columns=['date'], inplace=True)
+
+            # Calculate saturation vapor pressure (vp_sat) in kPa
+            merged_df['vp_sat_kPa'] = 0.61078 * np.exp((17.27 * merged_df['meant']) / (merged_df['meant'] + 237.3))
+            # Convert vp_sat to hPa
+            merged_df['vp_sat_hPa'] = merged_df['vp_sat_kPa'] * 10
+            # Convert vp_deficit from kPa to hPa (if your vp_deficit is in kPa)
+            merged_df['vp_deficit_hPa'] = merged_df['vp_deficit'] * 10
+            # Calculate actual vapor pressure (vp) in hPa
+            merged_df['vp'] = merged_df['vp_sat_hPa'] - merged_df['vp_deficit_hPa']
+
+            merged_df['vpd'] = merged_df["vp_deficit"]
+            # Drop intermediate columns (optional)
+            merged_df.drop(columns=['vp_sat_kPa', 'vp_sat_hPa', 'vp_deficit_hPa'], inplace=True)
+
+            # Reorder columns as desired
+            new_order = ['year', 'day', 'rain', 'maxt', 'mint', 'meant', 'soilt', 'rhmean', 'vp', 'vpd','windspeed', 'radn']
+
+            dataframes_array[i] = merged_df[new_order]
+
+        # delete the last element of the historical df because this day already exists in the forcast df
+        if(range(len(urls)) == 2):
+            print("Deleting duplicate")
+            dataframes_array[0].drop(dataframes_array[0].index[-1], inplace=True)
+
+        # merge the 2 dataframes
+        print(dataframes_array)
+        final_df = pd.concat(dataframes_array, ignore_index=True)
 
         # Save the final DataFrame to CSV
         final_df.to_csv(self.csv_filename, index=False)
@@ -170,8 +250,8 @@ if __name__ == "__main__":
         location="Heraklion",
         latitude=35.341846,
         longitude=25.148254,
-        start_date="2023-01-01",
-        end_date="2025-01-01",
+        start_date="2025-03-10",
+        end_date="2025-03-30",
         csv_filename="Heraklion.csv",
         ini_filename="Heraklion.ini"
     )
