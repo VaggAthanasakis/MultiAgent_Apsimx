03552ff085f844f610677f3e0eb142372585236e
VaggAthanasakis <athanasakis.evangelos@outlook.com>
2025-04-09 20:03:31 +0300

    need changes in the tools for proper yield and water

--- supervisor_v2.py
+++ supervisor_v2.py
@@ -170,16 +170,15 @@ def get_sensor_data_tool(crop: str):
     # Default sensor data if none provided
 
     sensor_data = [
-        ("2025-04-5", 0, 5),
-        ("2025-04-6", 0, 5),
-        ("2025-04-7", 0, 5),
-        ("2025-04-8", 0, 5),
-        ("2025-04-9", 0, 2),
-        ("2025-04-10", 0, 5),
-        ("2025-04-11", 0, 1),
-        ("2025-04-12", 0, 2),
-        ("2025-04-13", 0, 2),
-        ("2025-04-14", 0, 2),
+        ("2025-04-1", 0, 20),
+        ("2025-04-2", 0, 20),
+        ("2025-04-3", 0, 12),
+        ("2025-04-4", 0, 23),
+        ("2025-04-5", 0, 32),
+        ("2025-04-6", 0, 23),
+        ("2025-04-7", 0, 23),
+        ("2025-04-8", 0, 20),
+        ("2025-04-9", 0, 20),
     ]
 
     # ​To convert volumetric water content (VWC) to millimeters (mm) of water within a specific soil depth, 
@@ -295,6 +294,9 @@ def get_api_data_tool(field_id: int):
     #     except (ValueError, TypeError):
     #         # Keep as string if conversion fails
     #         soil_analysis[prop_name] = value["value"]
+
+
+    # get soil Analysis values
     soil_analysis = {}
     try:
         soil_values = data.get("farmland", {}).get("latest_soilanalysis", {}).get("soilanalysis_values", [])
@@ -330,9 +332,13 @@ def get_api_data_tool(field_id: int):
     # get these values in order to return them
     crop =  data.get("croptype", {}).get("name")
     growth_type = data.get("croptype", {}).get("growth_type")
-    clay = soil_analysis.get("clay")
-    sand = soil_analysis.get("sand")
-    silt = soil_analysis.get("silt")
+
+    clay = soil_analysis.get("clay") if soil_analysis.get("clay") is not None else 35
+    sand = soil_analysis.get("sand") if soil_analysis.get("sand") is not None else 35
+    silt = soil_analysis.get("silt") if soil_analysis.get("silt") is not None else 100-clay-sand
+
+    planting_date = data.get("start_date") # may need to be data.get("sowing_date")
+
 
     extracted_data = {
         # get() without default returns None if keys are missing
@@ -344,7 +350,7 @@ def get_api_data_tool(field_id: int):
         "Clay": clay,
         "Sand": sand,
         "Silt": silt,
-        "pH": soil_analysis.get("ph"),
+        "pH": soil_analysis.get("ph") if soil_analysis.get("ph") is not None else 7.5,
         "BD": 1.16,                  ## We have to take the below values from the api
         "LL15": 0.28,
         "DUL": 0.36,
@@ -358,6 +364,7 @@ def get_api_data_tool(field_id: int):
         "SoilCNRatio": 7.44,
         "InitialCNR": 7.44,
         "location": "Heraklion",
+        "planting_date": planting_date,
     }
 
     # Check if we have a annual or a perennial crop
@@ -441,6 +448,7 @@ def command_file_format(start_date: str, end_date: str):
     cn_ratio = data_json.get("SoilCNRatio")
     start_age = data_json.get("Start_Age")
     crop = data_json.get("crop_type")
+    planting_date = data_json.get("planting_date")
 
     weather_csv = location + ".csv"
     weather_ini = location + ".ini"
@@ -505,6 +513,9 @@ def command_file_format(start_date: str, end_date: str):
             "[Soil].Chemical.CEC[1:6]": CEC,
             "[Soil].Chemical.EC[1:6]": EC,
             f"[{crop}Soil].LL[1:6]": LL,
+            "[Sow].Script.PlantingDate": planting_date,
+            # "[Sow].Script.PlantingDepth": planting_depth,
+            # "[Sow].Script.RowSpacing": row_space,
         })
     
     with open(commands_file, "r") as file:
@@ -636,6 +647,8 @@ crop_simulator_agent = create_react_agent(
     AFTER THE TOOLS EXECUTION:
     -> You have to reason about the data and give a response to the farmer.
     -> Consider and include in the response the data that you have received from the tools like: water applied, weather forecast, percentages of sand-silt-clay of the Soil Texture, the crop and the growth type.
+    -> If you think that the simulation gives a smaller amount of water than the optimal amount of water, you have to give a recommendation to the farmer about how much water he has to apply.
+    -> In order to be more accurate, you have to take into consideration the soil texture (sand, silt, clay) and the weather forecast (rainfall).
     -> The response must be easily understandable by a farmer.
     -> Do not include polite closing remarks or encouragements to ask more questions. End responses directly with the relevant information.
 
@@ -705,6 +718,7 @@ greek_translator_agent = create_react_agent(
 def crop_simulator_agent_node(state: State) -> Command[Literal["supervisor"]]:
     print("\nInside Crop Simulator Node")
     result = crop_simulator_agent.invoke(state)
+    print(f"\n\nRESULT: {result["messages"][-1].content}\n")
 
     new_progress = state.get("progress", []) + ["crop_simulator"]
    
@@ -764,8 +778,15 @@ def multiagent_Apsimx_Simulator(prompt: str):
 
 
 # This will be the prompt that the user will give to the system from the frontend
+# user_prompt = """
+#     1) Create a crop simulation in the field with id = 62 for the period starting from today and for the next 7 days.
+#     2) Analyse the Data of the simulation in order to output the total water that the simulation is recommends to apply.
+#     3) Give some advice for the crop to the farmer for the next days in greek.
+
+# """
+
 user_prompt = """
-    1) Create a crop simulation in the field with id = 62 for the period starting from today and for the next 7 days.
+    1) Create a crop simulation in the field with id = 62 for the period starting from 10-10-2024 until 10-10-2025.
     2) Analyse the Data of the simulation in order to output the total water that the simulation is recommends to apply.
     3) Give some advice for the crop to the farmer for the next days in greek.
 
